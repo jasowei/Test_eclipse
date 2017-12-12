dc48a7d68a25d6170075b4854bc92cf43368aee4
Jaso0oo000oo0o <jasowei.@163.com>
2017-12-12 16:13:18 +0800

    Test push
    jaso

---
+++ src/com/baidu/Hello.java
@@ -0,0 +1,22 @@
+package com.baidu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//@WebServlet("/hello")
+public class Hello extends HttpServlet {
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		writer.write("<h1>shawanyier</h1>");
+		
+	}
+}
